17b6d5b1a8301856f04eea11ad5b04f74a324b15
Manikandan Ramakrishnan <manikandan.ramakrishnan@inmar.com>
2016-01-08 16:35:33 -0500

    Simple input/output, string concat, str-int conversion

---
+++ helloname.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+
+print('What is your name?') # ask for name
+myName = input()
+print('Nice to meet you, ' + myName)
+print('Your name is only ' + str(len(myName)) + ' long!')
+
+print('How old are you?') # ask for age
+myAge = input()
+print('Wow! You are going to be ' + str(int(myAge) + 1) + ' real soon!')
+
