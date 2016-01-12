a3da69f83abf26f912315aaf13107410b723fe04
Manikandan Ramakrishnan <manikandan.ramakrishnan@inmar.com>
2016-01-11 21:34:05 -0500

    added elif statement

--- if_else_example.py
+++ if_else_example.py
@@ -5,5 +5,7 @@ print('Nice to meet you, ' + uName + '!')
 
 if len(uName) >= 7:
     print("And btw, you should probably shorten your name...")
+elif len(uName) == 0:
+    print("You poor thing! You were never named!")
 else:
     print("And btw, are you sure you are not as small as your name? *wink* *wink*")
