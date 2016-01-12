b020f1ca95e710e0283e6d1eb1ff70518fdcd0c9
Manikandan Ramakrishnan <manikandan.ramakrishnan@inmar.com>
2016-01-11 21:23:36 -0500

    Simple if/else program

---
+++ if_else_example.py
@@ -0,0 +1,9 @@
+# Gets name and greets user depending on the length of the name
+print("Hello! I'm Chitti. What's your name?")
+uName = input()
+print('Nice to meet you, ' + uName + '!')
+
+if len(uName) >= 7:
+    print("And btw, you should probably shorten your name...")
+else:
+    print("And btw, are you sure you are not as small as your name? *wink* *wink*")
